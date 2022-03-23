940fb3403c043ea8ba6c24cd07571d301022b6df
Imtiyazdeveloper <imtiyazmd803@gmail.com>
2022-03-23 18:18:57 +0530

    Backgroun changer app

---
+++ 7. Background Color Change/bg_changer/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "BG Changer",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Random Background"),
+        ),
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+// HomePage Class
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageStateState createState() => _HomePageStateState();
+}
+
+class _HomePageStateState extends State<HomePage> {
+  var colors = [
+    Colors.amber,
+    Colors.black,
+    Colors.blue,
+    Colors.green,
+    Colors.red,
+    Colors.pink,
+    Colors.orange // or Color(0xff)
+  ];
+
+  var currenColor = Colors.white;
+
+  setRandomColor() {
+    var rnd = Random().nextInt(colors.length);
+    setState(() {
+      currenColor = colors[rnd];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: currenColor,
+      child: Center(
+        child: ElevatedButton(
+          child: Text('Press Change'),
+          onPressed: setRandomColor,
+          style: ElevatedButton.styleFrom(
+              primary: Colors.teal,
+              padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+              textStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+        ),
+      ),
+    );
+  }
+}
